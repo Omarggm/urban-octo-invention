8532367e5ac765a58d63b24fd2c0330e8c584698
Omar <Omarggm@gmail.com>
2023-03-12 18:42:02 -0700

    add/retrieve code from local storage

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html. -COMPLETED-
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time? -COMPLETED_
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page. -COMPLETED-
-
-
-
 $(document).ready(function () {
-  //displays current date
-  var currentDate = dayjs().format("dddd MMMM D");
-  $("#currentDay").text(currentDate);
-  $('.time-block').each(changeBg);
+  var currentDate = dayjs().format("dddd MMMM D"); //pulls data on current date and formats it
+  $("#currentDay").text(currentDate); //displays current date
 
+  $('.time-block').each(changeBg); // runs changeBg funnction on all classes with time-block
 
+  // Load data from local storage
+  $('.description').each(function () {
+    var id = $(this).parent().attr('id');
+    var data = localStorage.getItem(id);
+    if (data) {
+      $(this).val(data);
+    }
+  });
 
-  function changeBg() {
+  // Save data to local storage on change event
+  $('.description').on('change', function () {
+    var id = $(this).parent().attr('id');
+    var data = $(this).val();
+    localStorage.setItem(id, data);
+  });
+
+  function changeBg() {  //function to change background color of the row depending on the time of day
     var currentHour = dayjs().format("H"); //gets data for current hour
     var timeId = parseInt($(this).attr('id').split('-')[1]); // pulls data time-block via the id to get a number 
     if (currentHour < timeId) {
       $(this).addClass("future");
     } else if (currentHour == timeId) {
       $(this).addClass("present");
-    }
-    else if (currentHour > timeId) {
+    } else if (currentHour > timeId) {
       $(this).addClass("past");
     }
   };
-
-
-
 });
